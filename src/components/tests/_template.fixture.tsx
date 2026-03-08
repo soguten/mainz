a04c36faf0d66f39dba0e591a6c2f4d99df12c1d
Alexandre <alexandretavares@outlook.com>
2026-03-08 14:43:40 -0300

    Add test templates file

---
+++ src/components/tests/_template.fixture.tsx
@@ -0,0 +1,15 @@
+import { Component } from "mainz";
+
+export class ExampleComponent extends Component<{}, { count: number }> {
+    protected override initState() {
+        return { count: 0 };
+    }
+
+    override render(): HTMLElement {
+        return (
+            <button type="button" onClick={() => this.setState({ count: this.state.count + 1 })}>
+                {String(this.state.count)}
+            </button>
+        );
+    }
+}
